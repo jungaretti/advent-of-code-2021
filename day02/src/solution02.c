5a0650ad6a32679bbcfa3ef0bf4659f06cc104dc
JP Ungaretti <hello@jungaretti.com>
2021-12-04 08:49:30 +0000

    Solve day 2, part 2

--- day02/src/solution02.c
+++ day02/src/solution02.c
@@ -33,11 +33,43 @@ int measure_movement(FILE *file)
     return horizontal * vertical;
 }
 
+int measure_movement_withaim(FILE *file)
+{
+    int horizontal = 0;
+    int vertical = 0;
+    int aim = 0;
+    char line[LINE_BUFFER_SIZE];
+    while (fgets(line, LINE_BUFFER_SIZE, file) != NULL)
+    {
+        char *direction = strtok(line, " ");
+        int vector = atoi(strtok(NULL, "\n"));
+
+        if (direction[0] == 'u')
+        {
+            aim -= vector;
+        }
+        else if (direction[0] == 'd')
+        {
+            aim += vector;
+        }
+        else
+        {
+            // Who needs error handling?
+            horizontal += vector;
+            vertical += vector * aim;
+        }
+    }
+
+    return horizontal * vertical;
+}
+
 int main(int argc, char const *argv[])
 {
     FILE *file = fopen(argv[1], "r");
 
     printf("Part One: %i\n", measure_movement(file));
+    rewind(file);
+    printf("Part Two: %i\n", measure_movement_withaim(file));
 
     return 0;
 }
